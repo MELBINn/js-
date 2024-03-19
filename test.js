56382b151140057f341b13418b5c3b10bb41ddb8
MELBINn <melbinsunny29@gmail.com>
2024-03-19 23:44:58 +0530

    update

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-let result =((a,b,c)=>{
-  return a + (-b+c)
-})
-console.log(result(5,2,3))
+// Using Math.pow
+function squareUsingMathPow(number) {
+  return Math.pow(number, 2);
+}
+
+// Example usage
+const result1 = squareUsingMathPow(5);
+console.log(result1); // Output: 25
 
 
-let result =((a,b,c)=>{
-  return a + (-b+c)
-})
-console.log(result(5,2,3))
+// Using the exponentiation operator
+function squareUsingExponentiation(number) {
+  return number ** 2;
+}
+
+// Example usage
+const result2 = squareUsingExponentiation(5);
+console.log(result2); // Output: 25
+
+//   Certainly! The Math.pow function in JavaScript is used to raise a base to the power of an exponent. The syntax is as follows:
+
+Math.pow(base, exponent);
